080244d864e4eec27ae4107c57c9a162f260533a
Robert Giles <robert.ryan.giles@gmail.com>
2018-04-01 10:37:56 -0500

    Changed Startup Song to 1-Up and Qwerty Song to Zelda Puzzle

--- keyboards/preonic/keymaps/donkthelonk/config.h
+++ keyboards/preonic/keymaps/donkthelonk/config.h
@@ -4,11 +4,11 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(IMPERIAL_MARCH)
+    #define STARTUP_SONG SONG(ONE_UP_SOUND)
     // #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+    #define DEFAULT_LAYER_SONGS { SONG(ZELDA_PUZZLE), \
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
