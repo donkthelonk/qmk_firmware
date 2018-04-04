de96fbb0280cfa2e2984e95d690f2241dfcbcd81
Robert Giles <robert.ryan.giles@gmail.com>
2018-04-04 01:04:44 -0500

    Created custom keymap for RAMA M6-A

---
+++ keyboards/rama/m6_a/keymaps/donkthelonk/keymap.c
@@ -0,0 +1,14 @@
+#include "../../m6_a.h"
+
+#define LOCK LGUI(KC_L)
+#define SAVE LCTL(KC_S)
+#define COPY LCTL(KC_C)
+#define PASTE LCTL(KC_V)
+#define CALC KC_CALC
+#define MUTE KC_MUTE
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	KEYMAP(
+		CALC, MUTE, SAVE, COPY, PASTE, LOCK)
+};
