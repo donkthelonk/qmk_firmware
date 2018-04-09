1916ca0b750950a92a9e8711c7836f3d9b5e0efb
Robert Giles <robert.ryan.giles@gmail.com>
2018-04-09 01:29:16 -0500

    Change startup song to Imperial March and QWERTY song to Sonic Ring

--- keyboards/preonic/keymaps/donkthelonk/config.h
+++ keyboards/preonic/keymaps/donkthelonk/config.h
@@ -4,11 +4,11 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(ONE_UP_SOUND)
+    #define STARTUP_SONG SONG(IMPERIAL_MARCH)
     // #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(ZELDA_PUZZLE), \
+    #define DEFAULT_LAYER_SONGS { SONG(SONIC_RING), \
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
