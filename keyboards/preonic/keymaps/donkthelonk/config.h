7acb97554dcb4b9087960f26ba2837b077e0b680
Robert Giles <robert.ryan.giles@gmail.com>
2018-07-18 18:29:29 -0500

    idk man

--- keyboards/preonic/keymaps/donkthelonk/config.h
+++ keyboards/preonic/keymaps/donkthelonk/config.h
@@ -9,7 +9,7 @@
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(SONIC_RING), \
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
